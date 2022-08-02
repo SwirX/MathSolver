4bff8e4f223dec6dcd12501475841166fd646557
SwirX <92632751+SwirX@users.noreply.github.com>
2022-08-02 23:14:05 +0200

    Delete javascript directory

--- javascript/main.js
+++
@@ -1,8 +0,0 @@
-const theme_toggle = document.querySelector(".theme-toggle");
-const body = document.querySelector('body');
-
-
-theme_toggle.onclick = ()=>{
-    body.classList.toggle('darkmode');
-}
-
